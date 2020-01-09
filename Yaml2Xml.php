e5ab5a5e8d92e6447ee90c2a07147f6f9b2f93af
Nico <nicolas.alonso81@gmail.com>
2020-01-09 21:04:24 +0100

    Initial commit

---
+++ Yaml2Xml.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace nk;
+
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Yaml 2 XML
+ */
+class Yaml2XML
+{
+
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        # code...
+    }
+}
